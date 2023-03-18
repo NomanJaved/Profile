4087d51d1c800f0865664195c06f7386419d633b
NomanJaved <nomanjaved2011@gmail.com>
2023-03-18 18:52:07 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+const profile = {
+    name: 'Noman Javed',
+    email: 'nomanjaved2011@gmail.com',
+    education: 'MS in Software Engineering ~ 2016-2020',
+    experience: {
+        duration: '5 years',
+        domain: 'NGINX & Web development | integrations|deployments'
+    },
+    skills: { 
+        cloud: [ 'AWS'],
+        frontend: [ 'Bootstrap4', 'HTML', 'CSS', 'Material UI', 'TailwindCSS' 'Bootstrap' ],
+        backend: [ 'Laravel', 'PHP', 'Codeigniter', 'RESTful' ],
+        database: [ 'MySQL', 'mongoDB' ],
+        testing: [ 'Test Cases' ],
+        tools: [ 'Sublime Text', 'PHP Storm', 'VS Code', 'Git', 'Linux|Ubuntu|Centos', 'Nginx', 'Let's Encrypt', 'SSH' ],
+        languages: [ 'PHP', 'JavaScript', 'C/C++' ],
+        selfObservation: [ 'Programmer', 'Critical Thinker', 'Problem Solver', 'Web Solutions Architect' ]
+    }
+}
